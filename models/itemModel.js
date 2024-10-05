22d612521d3da3d939f9d28260aa4ced447cba77
DiabloTheCheater <sai.prapamontol@gmail.com>
2024-10-05 18:14:08 +0700

    <Update> 40-50% Schema is finished <NOTE> User controllers are broken atm. Will fix soon

---
+++ models/itemModel.js
@@ -0,0 +1,43 @@
+// Item related models -> Done
+
+const mongoose = require("mongoose");
+const { ItemType } = require("../enum");
+
+// Item schema
+const itemSchema = new mongoose.Schema({
+    businessID: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    itemName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    itemDescription: {
+        type: String
+    },
+    itemType: {
+        type: Number,
+        enum: [
+            ItemType.PRODUCT,
+            ItemType.SERVICE
+        ]
+    },
+    quantityOnHand: {
+        type: Number,
+        required: true
+    },
+    unitType: {
+        type: String
+    },
+    itemImageUrl: {
+        type: String
+    }
+});
+
+const itemCreator = (collectionName) => {
+    return mongoose.model("item", itemSchema, collectionName);
+};
+
+module.exports = { itemCreator }; 
